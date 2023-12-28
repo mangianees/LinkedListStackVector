414311d4521db6ffc0dd012f840e87b4d6d48127
mangianees@gmail.com <mangianees@gmail.com>
2023-12-28 01:45:28 +0000

    Lowest and Highest added

--- src/com/anees/squarnumbers/SquareAllNumbers.java
+++ src/com/anees/squarnumbers/SquareAllNumbers.java
@@ -7,12 +7,27 @@ public class SquareAllNumbers {
 		  int square = 0;
 			for(int i=0; i<n.length;i++)
 				{
-				System.out.println(n[i]);
 				square = n[i] * n[i];
 				tempString = tempString + Integer.toString(square) + "-";
 				}
 			return tempString;
 
-}
+	  	}
+	  
+	  public String LowestNumber(int n[])
+	  {
+		  int lowest=n[0];
+		  int greatest = n[1];
+			
+			for(int i=0; i<n.length - 1;i++)
+			{
+				if(lowest > n[i+1])
+					lowest = n[i+1];
+				else
+					greatest = n[i+1];
+			}
+			return Integer.toString(lowest) + " " + Integer.toString(greatest);
+			
+	  }
 
 }
--- src/com/anees/squarnumbers/SquareNumbers.java
+++ src/com/anees/squarnumbers/SquareNumbers.java
@@ -4,8 +4,9 @@ public class SquareNumbers {
 
 	public static void main(String[] args) {
 		SquareAllNumbers obj  = new SquareAllNumbers();
-		int[] n = {2,3,4,5,6};
+		int[] n = {2,3,4,-5,-6,14};
 		System.out.println(obj.squareDigits(n));
+		System.out.println(obj.LowestNumber(n));
 		}
 
 }

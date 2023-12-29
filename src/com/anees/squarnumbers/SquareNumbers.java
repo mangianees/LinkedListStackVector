5fd229c88ef6bbf6c427d0945e8dc3d0e89f1bb1
mangianees@gmail.com <mangianees@gmail.com>
2023-12-29 23:09:32 +0000

    LinkedList Stack and Vector

--- src/com/anees/squarnumbers/SquareNumbers.java
+++ src/com/anees/squarnumbers/SquareNumbers.java
@@ -3,11 +3,16 @@ package com.anees.squarnumbers;
 public class SquareNumbers {
 
 	public static void main(String[] args) {
+		String Str="Anees Ahmed Mangi";
 		SquareAllNumbers obj  = new SquareAllNumbers();
-		int[] n = {12,3,-54,-5,6,14};
-		System.out.println(obj.squareDigits(n));
-		System.out.println(obj.LowestNumber(n));
-		System.out.println(obj.BubbleSort(n));
+//		int[] n = {12,3,-54,-5,6,14};
+//		System.out.println(obj.squareDigits(n));
+//		System.out.println(obj.LowestNumber(n));
+//		System.out.println(obj.BubbleSort(n));
+	
+		System.out.println(Str.charAt(Str.length()/2));
+	
+	
 	}
 
 }
